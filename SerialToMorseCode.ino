f84e239c251ca64ff8bd0d785b887c393650d412
André von Kugland <kugland@gmail.com>
2019-01-22 06:26:07 -0200

    Update SerialToMorseCode.ino

--- SerialToMorseCode.ino
+++ SerialToMorseCode.ino
@@ -26,38 +26,24 @@
 
 
 /* Table containing the ASCII code of the character and a descriptor byte with size
-   and binary-encoded Morse code (size occupies the 3 most-significant bits, while
-   the code occupies the least-significant 5 bits). */
+   and binary-encoded Morse code.  Bits 0-7 hold the character, bits 8-12 hold the
+   Morse code dots and dashes, and bits 13-15 hold the number of dots and dashes of
+   the Morse code. */
+
 
 /* Some pre-processor macros to help create the aforementioned table. */
-#define DOT  1
-#define DASH 0
-#define PACK_MORSE_N(ch, sz, mask)        { ch, (sz<<5) | (mask) }
-#define PACK_MORSE_1(ch, a)               PACK_MORSE_N(ch, 1, a)
-#define PACK_MORSE_2(ch, a, b)            PACK_MORSE_N(ch, 2, a|(b<<1))
-#define PACK_MORSE_3(ch, a, b, c)         PACK_MORSE_N(ch, 3, a|(b<<1)|(c<<2))
-#define PACK_MORSE_4(ch, a, b, c, d)      PACK_MORSE_N(ch, 4, a|(b<<1)|(c<<2)|(d<<3))
-#define PACK_MORSE_5(ch, a, b, c, d, e)   PACK_MORSE_N(ch, 5, a|(b<<1)|(c<<2)|(d<<3)|(e<<4))
-
-
-
-/* A nice 16-bit struct for the table. */
-typedef struct {
-  char idx_ch;                 /* Character */
-  byte data;                   /* Corresponding Morse code */
-} morse_code_t;
-
-
-/* A struct to hold timed digital signals (HIGHs and LOWs). */
-typedef struct {
-  unsigned long startMillis;   /* Send signal when millis() >= starMillis */
-  byte value;                  /* LOW or HIGH */
-} signal_t;
-
+#define DOT                               0
+#define DASH                              1
+#define PACK_MORSE_N(ch, sz, mask)        (((((sz<<5)|(mask))&255)<<8)|(ch&255))
+#define PACK_MORSE_1(ch, a)               PACK_MORSE_N(ch, 1, (a))
+#define PACK_MORSE_2(ch, a, b)            PACK_MORSE_N(ch, 2, (a)|(b<<1))
+#define PACK_MORSE_3(ch, a, b, c)         PACK_MORSE_N(ch, 3, (a)|(b<<1)|(c<<2))
+#define PACK_MORSE_4(ch, a, b, c, d)      PACK_MORSE_N(ch, 4, (a)|(b<<1)|(c<<2)|(d<<3))
+#define PACK_MORSE_5(ch, a, b, c, d, e)   PACK_MORSE_N(ch, 5, (a)|(b<<1)|(c<<2)|(d<<3)|(e<<4))
 
 
 /* The actual table, marked as PROGMEM to spare precious RAM. */
-const morse_code_t morse_code_tbl[36] PROGMEM = {
+const unsigned short morse_code_tbl[37] PROGMEM = {
   PACK_MORSE_1('E', DOT),
   PACK_MORSE_1('T', DASH),
   PACK_MORSE_2('A', DOT,  DASH),
@@ -93,13 +79,22 @@ const morse_code_t morse_code_tbl[36] PROGMEM = {
   PACK_MORSE_5('6', DASH, DOT,  DOT,  DOT,  DOT),
   PACK_MORSE_5('7', DASH, DASH, DOT,  DOT,  DOT),
   PACK_MORSE_5('8', DASH, DASH, DASH, DOT,  DOT),
-  PACK_MORSE_5('9', DASH, DASH, DASH, DASH, DOT)
+  PACK_MORSE_5('9', DASH, DASH, DASH, DASH, DOT),
+  0
 };
 
 
 
+/* A struct to hold timed digital signals (HIGHs and LOWs). */
+typedef struct {
+  unsigned long startMillis;   /* Send signal when millis() >= starMillis */
+  byte value;                  /* LOW or HIGH */
+} signal_t;
+
+
+
 /* A circular buffer to store characters. */
-CircularBuffer<char, 1536> charBuffer;
+CircularBuffer<byte, 1536> charBuffer;
 
 /* Circular buffer used to store signals. 11 is the max number
    of signals within a character. */
@@ -136,42 +131,48 @@ void enqueue_signals_from_char()
     return;
   }
 
-  char ch = charBuffer.shift(); /* Get the next char. */
+  byte           ch = charBuffer.shift();   /* Get the next char. */
+  unsigned long  startMillis = millis();    /* Starting time of the first signal. */
+  
+  /* Whitespace */
+  if (ch == 0x20 || (ch >= 0x09 && ch <= 0x0c)) {
+      /* Seven time units of silence between words. But why only
+         four here? There are already one after each dot or dash,
+         and two more after each letter. */
+      signalBuffer.push(signal_t{startMillis + 4 * MORSE_TIME_UNIT, LOW});
+      return;
+  }
 
   /* Convert lowercase to uppercase. */
   if (ch >= 'a' && ch <= 'z')
-    ch -= 32;
+    ch -= 'a' - 'A';
 
-  if (ch == ' ' || ch == '\t' || ch == '\n') {
-    /* Seven time units of silence between words. But why only
-       four here? There are already one after each dot or dash,
-       and two more after each letter. */
-    signalBuffer.push(signal_t{millis() + 4 * MORSE_TIME_UNIT, LOW});
-    return;
-  }
+  const unsigned short *morse_code_tbl_ptr = morse_code_tbl;
 
-  for (int i = 0; i < 36; i++) {
+  while (1) {
     /* Load both idx and data from program memory. */
-    char idx_ch = pgm_read_byte(&(morse_code_tbl[i].idx_ch));
+    unsigned short morse_data = pgm_read_word(morse_code_tbl_ptr++);
 
-    if (ch == idx_ch) {
-      unsigned long startMillis = millis();
-      byte          data = pgm_read_byte(&(morse_code_tbl[i].data));
-      unsigned      size = (data >> 5) & 7;    /* 3 bits for size. */
-      data &= 31;                              /* 5 bits for dots and dashes. */
+    if (morse_data == 0)
+      break;
 
-      for (int j = 0; j < size; j++) {
+    if (ch == (morse_data & 255)) {             /* Character is in LSB. */
+      morse_data >>= 8;                         /* Morse code is in MSB. */
+      unsigned size = (morse_data >> 5) & 7;    /* 3 bits for size. */
+      morse_data &= 31;                         /* 5 bits for dots and dashes. */
+
+      while (size--) {
         /* First enqueue a high signal. */
         signalBuffer.push(signal_t{startMillis, HIGH});
         /* One time unit for dots, three for dashes. */
-        startMillis += (((data & 1) == DOT) ? 1 : 3) * MORSE_TIME_UNIT;
+        startMillis += (((morse_data & 1) == DOT) ? 1 : 3) * MORSE_TIME_UNIT;
         /* Then enqueue a low signal. */
         signalBuffer.push(signal_t{startMillis, LOW});
         /* One time unit of silence after each dot or dash.
            This silence need not be enqueued, we must only
            keep track of its timing, for the next signal. */
         startMillis += MORSE_TIME_UNIT;
-        data >>= 1;
+        morse_data >>= 1;
       }
       /* Three time units of silence after letter. (The third
          is the one after the dot or dash.) Now this must be
